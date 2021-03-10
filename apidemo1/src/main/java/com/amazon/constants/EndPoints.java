cc67041bc7616cedf172131d2130bf9260518d3e
vinayPesaru <vinay.pesaru@gmail.com>
2021-03-09 20:59:12 -0800

    Create EndPoints.java

---
+++ apidemo1/src/main/java/com/amazon/constants/EndPoints.java
@@ -0,0 +1,9 @@
+package com.amazon.constants;
+
+public class EndPoints {
+	public static final String BASE_URL ="http://dummy.restapiexample.com/api/v1";
+	public static final String GET_ALL_EMPLOYEES = "/employees";
+	public static final String GET_SINGLE_EMPLOYEE = "/employee/2";
+	public static final String DELETE_SINGLE_EMPLOYEE = "/delete/2";
+
+}
