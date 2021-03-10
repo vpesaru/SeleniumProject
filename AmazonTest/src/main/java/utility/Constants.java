ca7c6a92baac25b4029f55b56d67f7780decb7e2
vinayPesaru <vinay.pesaru@gmail.com>
2021-03-09 20:47:23 -0800

    Create Constants.java

---
+++ AmazonTest/src/main/java/utility/Constants.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class Constants {
+	  	public static final String URL = "https://www.amazon.com";
+	    public static final String User = "pesaru.vinay07@gmail.com";
+	    public static final String Password = "Abcd@1234";
+	    public static final String searchText = "qa testing for beginners";
+	    public static final int Wait=10;
+}
